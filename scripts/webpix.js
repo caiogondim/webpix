fceaaa05b86cc8deb2394691a75c9ce26ce1d2f8
Almir Filho <almirflorenciofilho@gmail.com>
2012-10-21 17:01:09 -0300

    Adiciona efeito Sharpen

--- scripts/webpix.js
+++ scripts/webpix.js
@@ -6,7 +6,8 @@ var webpix = ( function(){
 			'normal',
 			'grayscale',
 			'sepia',
-			'blur'
+			'blur',
+			'sharpen'
 		],
 		currentEffect: 0,
 		canvas: null,
@@ -186,6 +187,60 @@ var webpix = ( function(){
 				matrix[ i ] = 1/n
 
 			return this.convolute( data, matrix )
+		},
+
+		sharpen: function( data ){
+
+			return this.convolute( data,
+				[  0,  -1,  0,
+				   -1,  5, -1,
+				   0,  -1,  0 ]
+			)
+		},
+
+		convolute: function( pixels, weights, opaque ){
+
+			var side = Math.round(Math.sqrt(weights.length));
+			var halfSide = Math.floor(side/2);
+			var src = pixels;
+			var sw = this.canvas.width;
+			var sh = this.canvas.height;
+			// pad output by the convolution matrix
+			var w = sw;
+			var h = sh;
+			// go through the destination image pixels
+			var alphaFac = opaque ? 1 : 0;
+			for (var y=0; y<h; y++) {
+				for (var x=0; x<w; x++) {
+					var sy = y;
+					var sx = x;
+					var dstOff = (y*w+x)*4;
+					// calculate the weighed sum of the source image pixels that
+					// fall under the convolution matrix
+					var r=0, g=0, b=0, a=0;
+					for (var cy=0; cy<side; cy++) {
+						for (var cx=0; cx<side; cx++) {
+							var scy = sy + cy - halfSide;
+							var scx = sx + cx - halfSide;
+							if (scy >= 0 && scy < sh && scx >= 0 && scx < sw) {
+								var srcOff = (scy*sw+scx)*4;
+								var wt = weights[cy*side+cx];
+								r += src[srcOff] * wt;
+								g += src[srcOff+1] * wt;
+								b += src[srcOff+2] * wt;
+								a += src[srcOff+3] * wt;
+							}
+						}
+					}
+
+					src[dstOff] = r;
+					src[dstOff+1] = g;
+					src[dstOff+2] = b;
+					src[dstOff+3] = a + alphaFac*(255-a);
+				}
+			}
+
+			return src;
 		},
 
 		lixeira: function() {
