b2ccf9d5c9076a646278765ea287ae207a745c1a
Almir Filho <almirflorenciofilho@gmail.com>
2012-10-21 16:57:47 -0300

    Remove aplicação de efeitos ao vivo

--- scripts/webpix.js
+++ scripts/webpix.js
@@ -3,11 +3,16 @@ var webpix = ( function(){
 	var webpix = {
 
 		effects: [
-			'video-normal',
-			'video-sepia',
-			'video-blur'
+			'normal',
+			'grayscale',
+			'sepia',
+			'blur',
+			'sharpen',
+			'crazy'
 		],
 		currentEffect: 0,
+		canvas: null,
+		ctx: null,
 
 		init: function() {
 
@@ -42,49 +47,76 @@ var webpix = ( function(){
 		permissaoNegada: function() {
 			alert(
 				'É necessário que você habilite o acesso a câmera' +
-			 	'para que o experimento funcione')
+				'para que o experimento funcione')
 		},
 
-		efeitoNext: function() {
-			if (this.currentEffect == ( this.effects.length - 1) ) {
+		efeitoNext: function(){
+
+			if( this.currentEffect == (this.effects.length - 1) )
 				this.currentEffect = 0
-			} else {
+			else
 				this.currentEffect = this.currentEffect + 1
-			}
 
-			// adiciona a classe css
-			$( 'video' )
-				.removeClass()
-				.addClass( this.effects[this.currentEffect] )
+			this.aplicaEfeito()
 		},
 
-		efeitoPrev: function() {
-			if (this.currentEffect == 0 ) {
+		efeitoPrev: function(){
+
+			if( this.currentEffect == 0 )
 				this.currentEffect = this.effects.length - 1
-			} else {
+			else
 				this.currentEffect = this.currentEffect - 1
+
+			this.aplicaEfeito()
+		},
+
+		aplicaEfeito: function(){
+
+			this.ctx.drawImage( $( '.fotoOriginal' )[0], 0, 0 )
+			newBlob = this.ctx.getImageData( 0, 0, this.canvas.width, this.canvas.height )
+
+			switch( this.currentEffect ){
+				// normal
+				case 0: break
+				// grayscale
+				case 1:
+					newBlob.data = this.grayscale( newBlob.data )
+					break
+				// sepia
+				case 2:
+					newBlob.data = this.sepia( newBlob.data )
+					break
+				// blur
+				case 3:
+					newBlob.data = this.blur( newBlob.data )
+					break
+				// sharpen
+				case 4:
+					newBlob.data = this.sharpen( newBlob.data )
+					break
+
+				case 5:
+					newBlob.data = this.crazy( newBlob.data )
+					break
+
+				default: break
 			}
 
-			// adiciona a classe css
-			$( 'video' )
-				.removeClass()
-				.addClass( this.effects[this.currentEffect] )
+			this.ctx.putImageData( newBlob, 0, 0 )
+			this.atualizaFoto()
 		},
 
 		tirarFoto: function() {
-			var canvas = $( 'canvas' )[0]
-			canvas.width = $( 'video' )[0].videoWidth
-			canvas.height = $( 'video' )[0].videoHeight
-			var ctx = canvas.getContext( '2d' )
-			ctx.drawImage( $( 'video' )[0], 0, 0 )
 
-			$( '.foto' )
-				.attr( 'src', canvas.toDataURL('image/png') )
-				.show()
-			$( '.controle-download' )
-				.attr( 'href', canvas.toDataURL('image/png') )
-			$( 'video' )
-				.hide()
+			this.canvas = $( 'canvas' )[0]
+			this.canvas.width = $( 'video' )[0].videoWidth
+			this.canvas.height = $( 'video' )[0].videoHeight
+			this.ctx = this.canvas.getContext( '2d' )
+			this.ctx.drawImage( $( 'video' )[0], 0, 0 )
+			this.atualizaFoto()
+
+			$( '.fotoOriginal' ).attr( 'src', this.canvas.toDataURL('image/png') )
+			$( 'video' ).hide()
 
 			// atualiza controles
 			$( '.controle-gatilho' )
@@ -93,6 +125,32 @@ var webpix = ( function(){
 				.removeClass( 'controle-botao-disabled' )
 			$( '.controle-lixeira' )
 				.removeClass( 'controle-botao-disabled' )
+			$( '.efeito-prev' )
+				.removeClass( 'controle-botao-disabled' )
+			$( '.efeito-next' )
+				.removeClass( 'controle-botao-disabled' )
+		},
+
+		atualizaFoto: function(){
+
+			$( '.foto' )
+				.attr( 'src', this.canvas.toDataURL('image/png') )
+				.show()
+			$( '.controle-download' )
+				.attr( 'href', this.canvas.toDataURL('image/png') )
+		},
+
+		grayscale: function( data ){
+
+			for( i = 0; i < data.length; i += 4 ){
+				var r = data[ i ]
+				var g = data[ i+1 ]
+				var b = data[ i+2 ]
+				var v = 0.2126*r + 0.7152*g + 0.0722*b
+				data[ i ] = data[ i+1 ] = data[ i+2 ] = v
+			}
+
+			return data
 		},
 
 		lixeira: function() {
@@ -108,6 +166,10 @@ var webpix = ( function(){
 			$( '.controle-download' )
 				.addClass( 'controle-botao-disabled' )
 			$( '.controle-lixeira' )
+				.addClass( 'controle-botao-disabled' )
+			$( '.efeito-prev' )
+				.addClass( 'controle-botao-disabled' )
+			$( '.efeito-next' )
 				.addClass( 'controle-botao-disabled' )
 		}
 
